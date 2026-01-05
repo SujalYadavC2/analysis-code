46b4e26cad7d8ee5c4d7d7f518aadddbcd270386
SujalYadavC2 <sujalyadav91ind@gmail.com>
2026-01-05 22:08:17 +0100

    file_check function added to  check if pdb and traj file exists or not

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,25 @@
 import MDAnalysis as mda
 from tqdm import tqdm
+from pathlib import Path
+
+# Check if the pdf and trj file exist
+
+def file_check(pdb_file, traj_file):
+    pdb_path = Path(pdb_file)
+    traj_path = Path(traj_file)
+
+    pdb_check = pdb_path.exists()
+    traj_check = traj_path.exists()
+
+    if not pdb_check:
+        print(f"File path {pdb_path} does not exists")
+        return False
+    
+    elif not traj_check:
+        print(f"File path {traj_path} does not exists")
+        return False
+    else:
+        return True
 
 # First I need to create a function that can convert .dcd file to .xtc
 
