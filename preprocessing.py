009d9a7a4f07bf06668be96497b372f165a3ccfc
SujalYadavC2 <sujalyadav91ind@gmail.com>
2026-01-05 06:30:15 +0100

    dcd to xtc function written

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,14 @@
 import MDAnalysis as mda
-
+from tqdm import tqdm
 
 # First I need to create a function that can convert .dcd file to .xtc
 
-def dcd_to_xtc(dcd_file):
-    
-    ...
+def dcd_to_xtc(pdb_file, dcd_file):
+    u = mda.Universe(pdb_file, dcd_file)
+    ag = u.select_atoms('all')
+
+    with mda.Writer('traj.xtc', ag.n_atoms) as W:
+
+        for ts in tqdm(u.trajectory):
+            W.write(ag)
+
